1d1a6cddda28049d6a06a611bf01189085682cc7
Maria-Lisa <lisaloyce16@gmail.com>
2022-05-20 17:54:23 +0000

    _printf.c

--- main.h
+++ main.h
@@ -15,34 +15,24 @@
  *  @f: type pointer to function for the conversion specifier
  *
  */
-typedef struct format
+typedef struct code_format
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char *sc;
+	int (*f)(va_list);
+} code_f;
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
 
 #endif
